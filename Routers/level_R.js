003ea9dbc1807cbdc2df2b4ce1d0040b850679c1
osayl12 <hamedosayl@gmail.com>
2026-03-24 19:31:26 +0200

    fix typo

---
+++ Routers/level_R.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const level_Mid = require("../middleware/level_Mid");
+
+router.post("/Add", [language_Mid.AddItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK", Last_Id:res.insertId});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.put("/Update/:id", [language_Mid.UpdateItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK"});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.delete("/Delete", [language_Mid.DeleteItem], (req, res) => {
+    if(res.ok)
+        res.status(200).json({message:"OK"});
+    else
+        return res.status(500).json({message: res.err});
+});
+
+router.get("/List", [language_Mid.GetAllItems], (req, res) => {
+    if(res.ok) {
+        res.status(200).json(req.ItemsData);
+    }
+    else
+        return res.status(500).json({message: res.err});
+});
