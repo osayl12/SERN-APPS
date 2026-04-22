8f7596c900325824c6e655df4ba4d95baa2c9e57
osayl12 <hamedosayl@gmail.com>
2026-04-22 09:35:31 +0300

    UPDATE SQL

--- sfr_proj.sql
+++ sfr_proj.sql
@@ -1,155 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: מרץ 18, 2026 בזמן 12:44 PM
--- גרסת שרת: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+CREATE TABLE IF NOT EXISTS `questions` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`title` varchar(1000) NOT NULL,
+	`content` text NOT NULL,
+	`level_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS `q_level` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
+CREATE TABLE IF NOT EXISTS `language` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE IF NOT EXISTS `topics` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
---
--- Database: `sfr_proj`
---
+CREATE TABLE IF NOT EXISTS `lang_2_question` (
+	`q_id` int NOT NULL,
+	`lang_id` int NOT NULL
+);
 
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `language`
---
-
-CREATE TABLE `language` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `lang_2_question`
---
-
-CREATE TABLE `lang_2_question` (
-  `q_id` int(11) NOT NULL,
-  `lang_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `questions`
---
-
-CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
-  `title` varchar(1000) NOT NULL,
-  `content` text NOT NULL,
-  `level_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `q_level`
---
-
-CREATE TABLE `q_level` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `topics`
---
-
-CREATE TABLE `topics` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- מבנה טבלה עבור טבלה `topic_2_question`
---
-
-CREATE TABLE `topic_2_question` (
-  `q_id` int(11) NOT NULL,
-  `topic_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- אינדקסים לטבלה `language`
---
-ALTER TABLE `language`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- אינדקסים לטבלה `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- אינדקסים לטבלה `q_level`
---
-ALTER TABLE `q_level`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- אינדקסים לטבלה `topics`
---
-ALTER TABLE `topics`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `language`
---
-ALTER TABLE `language`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `questions`
---
-ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `q_level`
---
-ALTER TABLE `q_level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `topics`
---
-ALTER TABLE `topics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `topic_2_question` (
+	`q_id` int NOT NULL,
+	`topic_id` int NOT NULL
+);
